254059c38dec039d030fabca80783d8cdab7066a
Md. Mursalin Hasan Nirob <109570432+mursalin49@users.noreply.github.com>
2023-08-22 10:52:04 +0600

    Add files via upload

---
+++ column_row/main.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+import 'package:nirob/flutter%20widgets/appbar.dart';
+import 'package:nirob/flutter%20widgets/column_row/column_row.dart';
+import 'package:nirob/flutter%20widgets/container/container.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(primarySwatch: Colors.cyan),
+      color: Colors.green,
+      home: Home(),
+    );
+  }
+}
+
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        body: ListView(
+          children: [
+            Padding(
+              padding: EdgeInsets.only(top: 30, left: 100, right: 100),
+              child: MaterialButton(
+                onPressed: () {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => Appbars()));
+                },
+                child: Text("Appbar"),
+                color: Colors.green,
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(top: 30, left: 100, right: 100),
+              child: MaterialButton(
+                onPressed: () {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => Containers()));
+                },
+                child: Text("Containers"),
+                color: Colors.green,
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(top: 30, left: 100, right: 100),
+              child: MaterialButton(
+                onPressed: () {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => column_row()));
+                },
+                child: Text("column_row"),
+                color: Colors.green,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
